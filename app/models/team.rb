08741286b89cefddc78067381e8b886ea9b71de9
deke76 <darcy.lauder@shaw.ca>
2025-07-19 13:11:01 -0600

    add ice time associations to Team model; establish relationships for ice times and opponent ice times with appropriate dependent options

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ApplicationRecord
   validates_presence_of :name
   has_one_attached :logo, dependent: :destroy
   has_many :memberships, dependent: :destroy
+  has_many :ice_times, dependent: :destroy
+  has_many :opponent_ice_times, class_name: 'IceTime', foreign_key: 'opponent_id', dependent: :nullify
 
   def small_logo
     logo.variant(resize_to_limit: [100, 100]).processed
